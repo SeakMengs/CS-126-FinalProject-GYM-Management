289573f0662d001d8c3a68b45a912490f4ecae2d
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-08-02 00:32:12 +0700

    update

--- Gym_Portfolio.cpp
+++ Gym_Portfolio.cpp
@@ -4,12 +4,21 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <windows.h>
 using namespace std;
 
 #define maxx 50
 //? Function to clear console
 void clear() {
     system("cls");
+}
+
+//? Loading to make it cool
+void loading() {
+    for (int i = 0; i <= 5; i++) {
+        cout << ".";
+        Sleep(300);
+    }
 }
 
 //? Struct of user info it has {id, name, contact, subcription, time, pw}
@@ -28,7 +37,66 @@ struct gym {
     }
 };
 
+//? User Menu
+void userMenu() {
+    clear();
+    cout << "Hello";
+    Sleep(5000);
+}
+
+//? Admin Menu
+void adminMenu() {
+    clear();
+    cout << "Hello";
+    Sleep(5000);
+}
+
+//? Main Menu when start
+void mainMenu() {
+    string choiceInput;
+    char choice;
+    while (choice != '1' || choice != '2' || choice != 'x' || choice != 'X' )
+    {
+        clear();
+        cout << "**********************" << endl;
+        cout << "|GYM-MANGEMENT SYSTEM|" << endl;
+        cout << "**********************" << endl;
+        cout << "[1] User Mode" << endl;
+        cout << "[2] Admin Mode" << endl;
+        cout << "[X] Exit" << endl;
+        cout << endl;
+        cout << "Your choice: ";
+        //! This will get only the first letter the user enter
+        getline(cin, choiceInput);
+        if (choiceInput.size() == 1) {
+            choice = choiceInput[0];
+        }
+        if (choice == 'x' || choice == 'X')
+        {
+            clear();
+            cout << "Exiting";
+            loading();
+            clear();
+            cout << "Thank you for using our program :)";
+            break;
+        } 
+        switch (choice) {
+            case '1': 
+                userMenu();
+                break;
+            case '2': 
+                adminMenu();
+                break;
+            default:
+                cout << "\nPlease try again";
+                loading();
+                clear();
+        }
+    }
+}
+
 int main() {
     vector <gym> gymInfo;
+    mainMenu();
     return 0;
 }
